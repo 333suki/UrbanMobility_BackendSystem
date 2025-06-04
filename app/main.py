debdcb8d741bb3737962fb9b8bf24975ec0c4e28
333suki <sogaandmomayez@gmail.com>
2025-06-04 13:22:31 +0200

    Liddol DB start
    HELP

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-# docker build -t urban-mobility-system .
-# docker run -it --rm -v $(pwd)/data:/app/data urban-mobility-system
+from database import Database
+# docker build -t urban_mobility_system .
+# docker run -it --rm -v $(pwd)/data:/app/data urban_mobility_system
+# docker run -it --rm -v $(cd)/data:/app/data urban_mobility_system WUKI
 
 print("Hello World!")
+database = Database("data/database.db")
