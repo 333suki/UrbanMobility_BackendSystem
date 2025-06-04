05e6799e4d37eed1e67d36d2d28628b13979ff2a
333suki <sogaandmomayez@gmail.com>
2025-06-04 13:38:23 +0200

    ITWORKFRNOW

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from database import Database
 from login_screen import login_screen
 # docker build -t urban_mobility_system .
 # docker run -it --rm -v $(pwd)/data:/app/data urban_mobility_system
-# docker run -it --rm -v ./data:/app/data urban_mobility_system WUKI
+# docker run -it --rm -v ${PWD}/data:/app/data urban_mobility_system WUKI
 
 if __name__ == "__main__":
     print("Hello World!")
